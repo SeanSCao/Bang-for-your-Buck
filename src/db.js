f04db9d86fd762255ecd13e94cba61e5deab7af3
SeanSCao <ssc543@nyu.edu>
2018-04-02 20:53:03 -0400

    DBCONF

--- src/db.js
+++ src/db.js
@@ -3,25 +3,7 @@
 const mongoose = require('mongoose');
 const URLSlugs = require('mongoose-url-slugs');
 
-let dbconf;
-// is the environment variable, NODE_ENV, set to PRODUCTION? 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    // if we're in PRODUCTION mode, then read the configration from a file
-    // use blocking file io to do this...
-    const fs = require('fs');
-    const path = require('path');
-    const fn = path.join(__dirname, 'config.json');
-    const data = fs.readFileSync(fn);
-
-    // our configuration file will be in json, so parse it and set the
-    // connection string appropriately!
-    const conf = JSON.parse(data);
-    dbconf = conf.dbconf;
-} else {
-    // if we're not in PRODUCTION mode, then use
-    dbconf = 'mongodb://localhost/final';
-}
-
+let dbconf = 'mongodb://heroku_qtgdqqlg:j5r6gj90d8utr4p6oi5h8ggshk@ds231739.mlab.com:31739/heroku_qtgdqqlg';
 
 // my schema goes here!
 
